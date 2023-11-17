79d29ccf2f55985205ec8918624ec1dbfbc78ef7
gabrielhoux <gabriel.houx@gmail.com>
2023-11-17 11:56:20 +0100

    added 8080 port to app.run

--- app.py
+++ app.py
@@ -324,5 +324,5 @@ def search():
         return render_template("index.html")
 
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
